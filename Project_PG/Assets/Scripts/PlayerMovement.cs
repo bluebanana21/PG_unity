5adb03f037533a0e2f69a0230aa846516c0866bb
bluebanana21 <m.affanfahrozi@gmail.com>
2024-10-28 21:50:26 +0700

    update: prototype interact

--- Project_PG/Assets/Scripts/PlayerMovement.cs
+++ Project_PG/Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Yarn.Unity;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
     private float horizontal;
     private float vertical;
-    private float speed = 8f;
+    private float speed = 160f;
+    private float interactionRadius = 2f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
+    private DialogueAdvanceInput dialogueInput;
+
+    void Start()
+    {
+        dialogueInput = FindObjectOfType<DialogueAdvanceInput>();
+        dialogueInput.enableActionOnStart = false;
+    }
+
     void Update()
     {
+        
+        
+        if (FindObjectOfType<DialogueRunner>().IsDialogueRunning == true)
+        {
+            return;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CheckForNearbyNPC();
+            Debug.Log("Testing...");
+        }
+        
+
+        if (dialogueInput.enabled)
+        {
+            dialogueInput.enabled = false;
+            
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
         Flip();
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2 (horizontal * speed *Time.deltaTime, rb.velocity.y);
     }
 
     private void Flip()
@@ -34,6 +63,24 @@ public class PlayerMovement : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        }
+    }
+
+    public void CheckForNearbyNPC()
+    {
+        var allParticipants = new List<NPCInteractable>(FindObjectsOfType<NPCInteractable>());
+        var target = allParticipants.Find(delegate (NPCInteractable p)
+        {
+            return string.IsNullOrEmpty(p.talkToNode) == false && // has a conversation node?
+            (p.transform.position - this.transform.position)// is in range?
+            .magnitude <= interactionRadius;
+        });
+        if (target != null)
+        {
+            // Kick off the dialogue at this node.
+            FindObjectOfType<DialogueRunner>().StartDialogue(target.talkToNode);
+            // reenabling the input on the dialogue
+            
         }
     }
 }
