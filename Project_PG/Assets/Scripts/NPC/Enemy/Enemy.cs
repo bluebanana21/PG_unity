0780927d396cb7c0f12a0d396d890048cfec2ea7
bluebanana21 <m.affanfahrozi@gmail.com>
2024-11-05 14:06:49 +0700

    update: enemy movement

--- Project_PG/Assets/Scripts/NPC/Enemy/Enemy.cs
+++ Project_PG/Assets/Scripts/NPC/Enemy/Enemy.cs
@@ -12,13 +12,15 @@ public class Enemy : MonoBehaviour
 
     private int currentHealth;
 
-    // Start is called before the first frame update
+    public Transform player;
+    public bool isFlipped = false;
+   
     void Start()
     {
         currentHealth = maxHelath; 
     }
 
-    // Update is called once per frame
+    
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
@@ -39,5 +41,26 @@ public class Enemy : MonoBehaviour
 
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
+    }
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+
+        } else if (transform.position.x < player.position.x && !isFlipped)
+        {
+
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+
+        }
     }
 }
